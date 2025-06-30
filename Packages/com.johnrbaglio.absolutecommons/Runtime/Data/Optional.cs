2c6693a4279be4d428c338cfc80589cbff52b2aa
absoluteAquarian <jruss88@me.com>
2025-06-30 14:50:56 -0500

    actually implement Optional

--- Packages/com.johnrbaglio.absolutecommons/Runtime/Data/Optional.cs
+++ Packages/com.johnrbaglio.absolutecommons/Runtime/Data/Optional.cs
@@ -1,5 +1,24 @@
-﻿namespace AbsoluteCommons.Runtime.Data {
+﻿using AbsoluteCommons.Runtime.Attributes;
+using UnityEngine;
+
+namespace AbsoluteCommons.Runtime.Data {
 	[System.Serializable]
-	public class Optional {
+	public class Optional<T> {
+		[SerializeField] private bool hasValue;
+		[SerializeField, ReadOnlyIfTrue(nameof(Disabled))] private T value;
+
+		public bool Disabled => !hasValue;
+
+		public T Value => hasValue ? value : default;
+
+		public Optional() {
+			hasValue = false;
+			value = default;
+		}
+
+		public Optional(T value) {
+			hasValue = true;
+			this.value = value;
+		}
 	}
 }
